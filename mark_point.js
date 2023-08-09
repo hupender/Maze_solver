ef0b8442065ffdf9e723c76a38fe50fb791e79ec
hupender <hupenderkhatod>
2023-08-09 21:25:39 +0530

    mark using getImageData

--- mark_point.js
+++ mark_point.js
@@ -7,27 +7,21 @@ starting.onclick=function() {
         var x=Math.floor(event.clientX-coord.left);
         var y=Math.floor(event.clientY-coord.top);
         // create a new data pixel
-        var pixel=canvas_ctx.createImageData(1,1);
+        // var pixel=canvas_ctx.createImageData(1,1);
+        // pixel.data[0]=255;
+        // pixel.data[1]=0;
+        // pixel.data[2]=0;
+        // pixel.data[3]=255;
+        // canvas_ctx.putImageData(pixel,x,y);
+        // console.log("width is :" + pixel.width + " and height is :"+ pixel.height);
+
+        console.log("X is :"+ x+ " and Y is :" + y + " canavas is :"+ coord.height+ " and :"+ coord.width);
+        var pixel=canvas_ctx.getImageData(x,y,1,1);
         pixel.data[0]=255;
         pixel.data[1]=0;
         pixel.data[2]=0;
         pixel.data[3]=255;
         canvas_ctx.putImageData(pixel,x,y);
-        // console.log("width is :" + pixel.width + " and height is :"+ pixel.height);
-
-        console.log("X is :"+ x+ " and Y is :" + y + " canavas is :"+ coord.height+ " and :"+ coord.width);
-        // var pixel=canvas_ctx.getImageData(x,y,1,1);
-        // // var len=pixel.data;
-        // // var rec=canvas_ctx.getImageData(0,0,1,1);
-        // // rec[0]=0;
-        // // rec[1]=255;
-        // // rec[2]=0;
-        // // rec[3]=255;
-        // pixel[0]=255;
-        // pixel[1]=0;
-        // pixel[2]=0;
-        // pixel[3]=255;
-        // canvas_ctx.putImageData(pixel,x,y);
         var pixel=canvas_ctx.getImageData(x,y,1,1);
         var data=pixel.data;
         console.log(data.length);   
