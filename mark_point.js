c32de0865e881ea34bc6302f562b868d2e24c5f8
hupender <hupenderkhatod>
2023-08-09 21:23:10 +0530

    mark point using createImageData

--- mark_point.js
+++ mark_point.js
@@ -7,16 +7,14 @@ starting.onclick=function() {
         var x=Math.floor(event.clientX-coord.left);
         var y=Math.floor(event.clientY-coord.top);
         // create a new data pixel
-        // var pixel=canvas_ctx.createImageData(1,1);
-        // pixel[0]=255;
-        // pixel[1]=0;
-        // pixel[2]=0;
-        // pixel[3]=1;
-        // canvas_ctx.putImageData(pixel,x,y);
+        var pixel=canvas_ctx.createImageData(1,1);
+        pixel.data[0]=255;
+        pixel.data[1]=0;
+        pixel.data[2]=0;
+        pixel.data[3]=255;
+        canvas_ctx.putImageData(pixel,x,y);
         // console.log("width is :" + pixel.width + " and height is :"+ pixel.height);
 
-        // var x=event.clientX;
-        // var y=event.clientY;
         console.log("X is :"+ x+ " and Y is :" + y + " canavas is :"+ coord.height+ " and :"+ coord.width);
         // var pixel=canvas_ctx.getImageData(x,y,1,1);
         // // var len=pixel.data;
@@ -30,13 +28,14 @@ starting.onclick=function() {
         // pixel[2]=0;
         // pixel[3]=255;
         // canvas_ctx.putImageData(pixel,x,y);
+        var pixel=canvas_ctx.getImageData(x,y,1,1);
+        var data=pixel.data;
+        console.log(data.length);   
+        console.log("Color is " + data[0] + " "+ data[1]+ " "+data[2]+" " + data[3]);
+        
 
-        canvas_ctx.fillStyle="green";
-        canvas_ctx.fillRect(x,y,5,5);
-        // console.log(data.length);      
-        
-        // console.log("Color is " + data[0] + " "+ data[1]+ " "+data[2]+" " + data[3]);
-        
+        // canvas_ctx.fillStyle="green";
+        // canvas_ctx.fillRect(x,y,5,5);
     }
     
 
