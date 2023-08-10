f2286214f54c5d114c35420db526bed49694b522
hupender <hupenderkhatod>
2023-08-10 12:41:26 +0530

    only one point will be marked

--- mark_point.js
+++ mark_point.js
@@ -1,36 +1,32 @@
 var starting=document.querySelector("#start");
 var canvas=document.querySelector("#preview_image");
 var canvas_ctx=canvas.getContext("2d");
+// var st_color="green";
+
 starting.onclick=function() {
-    canvas.onclick=function(event) {
-        var coord=canvas.getBoundingClientRect();
-        var x=Math.floor(event.clientX-coord.left);
-        var y=Math.floor(event.clientY-coord.top);
-        // create a new data pixel
-        // var pixel=canvas_ctx.createImageData(1,1);
-        // pixel.data[0]=255;
-        // pixel.data[1]=0;
-        // pixel.data[2]=0;
-        // pixel.data[3]=255;
-        // canvas_ctx.putImageData(pixel,x,y);
-        // console.log("width is :" + pixel.width + " and height is :"+ pixel.height);
+    // canvas.addEventListener("click ",(event)=>mark_point(event));
+    // canvas.addEventListener("click",function(event) {mark_point(event)});
+    canvas.onclick=function(event) {mark_point(event)};
+}
 
-        // console.log("X is :"+ x+ " and Y is :" + y + " canavas is :"+ coord.height+ " and :"+ coord.width);
-        // var pixel=canvas_ctx.getImageData(x,y,1,1);
-        // pixel.data[0]=255;
-        // pixel.data[1]=0;
-        // pixel.data[2]=0;
-        // pixel.data[3]=255;
-        // canvas_ctx.putImageData(pixel,x,y);
-        // var pixel=canvas_ctx.getImageData(x,y,1,1);
-        // var data=pixel.data;
-        // console.log(data.length);   
-        // console.log("Color is " + data[0] + " "+ data[1]+ " "+data[2]+" " + data[3]);
-        
-
-        canvas_ctx.fillStyle="green";
-        canvas_ctx.fillRect(x,y,5,5);
-    }
+var x=-1,y=-1,pixel;
+function mark_point(event) {
     
+    // if a point has been marked and another click is detected then erase the previous one
+    if(x>=0 && y>=0) {
+        canvas_ctx.putImageData(pixel,x,y);
+    }
+    //get the canvas coordinates
+    var coord=canvas.getBoundingClientRect();
+    // get the coordinates of the marked point and subtract from canvas coordinates to get relative position
+    x=Math.floor(event.clientX-coord.left);
+    y=Math.floor(event.clientY-coord.top);
 
+    // get the orignal pixel so that if another click is detected the previous is restored
+    pixel=canvas_ctx.getImageData(x,y,5,5);
+    
+    // mark the starting point in green
+    canvas_ctx.fillStyle="green";
+    canvas_ctx.fillRect(x,y,5,5);
+    canvas.onclick=function(event) {mark_point(event)};
 }
